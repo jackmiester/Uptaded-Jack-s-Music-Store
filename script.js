94ea550e7a7eeb6113c06b5656a93a266cba9974
jackmiester <129291988+jackmiester@users.noreply.github.com>
2024-05-05 22:24:02 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+  document.getElementById("whiteib").onclick = function() {
+    window.location.href = "product1.html";
+  };
+
+  document.getElementById("blueb").onclick = function() {
+    window.location.href = "product2.html";
+  };
+
+  document.getElementById("blueib").onclick = function() {
+    window.location.href = "product3.html";
+  };
+
+  document.getElementById("redib").onclick = function() {
+    window.location.href = "product4.html";
+  };
+
+  document.getElementById("tel").onclick = function() {
+    window.location.href = "product5.html";
+  };
+
+  document.getElementById("bassyel").onclick = function() {
+    window.location.href = "product6.html";
+  };
+
+  document.getElementById("strat").onclick = function() {
+    window.location.href = "product7.html";
+  };
+
+  document.getElementById("lp").onclick = function() {
+    window.location.href = "product8.html";
+  };
